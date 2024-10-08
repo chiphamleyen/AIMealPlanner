5dc663b9f6a9b780dfc721b09e5e3b6ea1b60e98
Chi Pham <chiphamleyen@gmail.com>
2024-10-08 20:28:17 +1100

    Update signup login screens flow

--- app/src/main/java/com/example/nutrichief/view/LoginActivity.kt
+++ app/src/main/java/com/example/nutrichief/view/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.example.nutrichief.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrichief.MainActivity
@@ -29,11 +31,19 @@ class LoginActivity : AppCompatActivity() {
         .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         .build()
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val loginBtn = findViewById<Button>(R.id.login_emp_btn)
+
+        val signUpTextView = findViewById<TextView>(R.id.signUpTextView)
+        signUpTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         loginBtn.setOnClickListener {
             val email = findViewById<TextInputEditText>(R.id.login_email_field).text.toString()
